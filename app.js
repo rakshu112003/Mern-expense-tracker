59dd4b3dab840b9d006db1a6e0ad4185790100e5
Rakshitha HN <rakshug73@gmail.com>
2026-03-06 09:49:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+// Connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
+
+// Root test
+app.get("/", (req, res) => res.send("Server is running"));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`));

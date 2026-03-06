09c546dbe9f356d34e91294a8553cb76d10cfc15
Rakshitha HN <rakshug73@gmail.com>
2026-03-06 09:51:30 +0530

    Add files via upload

---
+++ Expense.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    user: {   // 👈 VERY IMPORTANT
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Expense", expenseSchema);
